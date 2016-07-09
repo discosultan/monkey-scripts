ecaf8bed8b68b9004f78757d0f2905530fba1557
Discosultan <j2v@neti.ee>
2016-07-10 01:06:41 +0300

    Update msg store

--- fb-message-store.js
+++ fb-message-store.js
@@ -18,10 +18,11 @@
     // ******* //
 
     var loggingEnabled = true;
-    var urlCheckInterval = 1000;
+    var initialSubscriptionTimeout = 1000;
+    var resubscriptionInterval = 5000;
     var requestedStorageQuotaMB = 100;
-    var sentMessagesStore = 'saadetud_sonumid.v1.html';
-    var actionOnRecentMessagesStore = 'tegevused_viimaste_sonumitega.v1.html';
+    var sentMessagesStore = 'saadetud_sonumid.v4.html';
+    var actionOnRecentMessagesStore = 'tegevused_viimaste_sonumitega.v4.html';
 
     // ******************* //
     // Event subscription. //
@@ -31,14 +32,22 @@
     var toDispose = []; // { target, type, listener, useCapture }
 
     // Resubscribe to events each time url change is detected.
-    setInterval(function() {
-        var currentUrl = location.href;
-        if (currentUrl !== lastUrl) {
-            unsubscribeFromEvents();
-            subscribeToEvents();
-        }
-        lastUrl = currentUrl;
-    }, urlCheckInterval);
+    // setInterval(function() {
+    //     var currentUrl = location.href;
+    //     if (currentUrl !== lastUrl) {
+    //         resetSubscriptions();
+    //     }
+    //     lastUrl = currentUrl;
+    // }, resubscriptionInterval);
+
+    // Resubscribe to events at regular intervals.
+    setInterval(resetSubscriptions, resubscriptionInterval);
+    setTimeout(resetSubscriptions, initialSubscriptionTimeout);
+
+    function resetSubscriptions() {
+        unsubscribeFromEvents();
+        subscribeToEvents();
+    }
 
     function unsubscribeFromEvents() {
         log('Unsubscribing from events...');
@@ -52,77 +61,101 @@
             log('Unsubscribed from ' + disposable.target + ' ' + disposable.type + '.');
         }
         toDispose.length = 0;
+
+        log('Unsubscribed from events.');
     }
 
     function subscribeToEvents() {
         log('Subscribing to events...');
 
-        var useCapture = false;
         var type = 'keydown';
-        var listener = onChatTabsPageletKeydown;
-        var chatTabsPageletEl = document.getElementById('ChatTabsPagelet');
-        if (chatTabsPageletEl) {
-            chatTabsPageletEl.addEventListener(type, listener, useCapture);
-            toDispose.push({ target: chatTabsPageletEl, type: type, listener: listener, useCapture: useCapture });
-            log('Subscribed to small chat window keydown.');
+        var listener = onSmallChatKeydown;
+        var useCapture = false;
+        var smallChatEl = getSmallChatEl();
+        if (smallChatEl) {
+            smallChatEl.addEventListener(type, listener, useCapture);
+            toDispose.push({ target: smallChatEl, type: type, listener: listener, useCapture: useCapture });
+            log('Subscribed to small chat keydown.');
         }
 
+        type = 'keydown';
+        listener = onLargeChatKeydown;
         useCapture = true;
-        listener = onPageletWebMessengerKeydown;
-        var pageletWebMessengerEl = document.getElementById('pagelet_web_messenger');
-        if (pageletWebMessengerEl) {
-            pageletWebMessengerEl.addEventListener(type, listener, useCapture);
-            toDispose.push({ target: pageletWebMessengerEl, type: type, listener: listener, useCapture: useCapture });
-            log('Subscribed to large chat window keydown.');
+        var largeChatEl = getLargeChatEl();
+        if (largeChatEl) {
+            largeChatEl.addEventListener(type, listener, useCapture);
+            toDispose.push({ target: largeChatEl, type: type, listener: listener, useCapture: useCapture });
+            log('Subscribed to large chat keydown.');
         }
 
-        useCapture = false;
+        // type = 'click';
+        // listener = onSmallChatHeaderClick;
+        // var smallChatHeaderEl = document.querySelector('#ChatTabsPagelet a.fbNubButton');
+        // log(smallChatHeaderEl);
+        // if (smallChatHeaderEl) {
+        //     smallChatHeaderEl.addEventListener(type, listener, useCapture);
+        //     toDispose.push({ target: smallChatHeaderEl, type: type, listener: listener, useCapture: useCapture });
+        //     log('Subscribed to small chat header click.');
+        // }
+
         type = 'click';
-        listener = onActionsBtnClick;
-        var largeChatActionsBtn = getLargeChatActionsBtn();
-        if (largeChatActionsBtn) {
-            largeChatActionsBtn.addEventListener(type, listener, useCapture);
-            toDispose.push({ target: largeChatActionsBtn, type: type, listener: listener, useCapture: useCapture });
-            log('Subscribed to actions button click.');
+        listener = onSmallChatActionsBtnClick;
+        useCapture = false;
+        var smallChatActionsBtnEl = getSmallChatActionsBtnEl();
+        if (smallChatActionsBtnEl) {
+            smallChatActionsBtnEl.addEventListener(type, listener, useCapture);
+            toDispose.push({ target: smallChatActionsBtnEl, type: type, listener: listener, useCapture: useCapture });
+            log('Subscribed to small chat actions button click.');
+        }
+
+        listener = onLargeChatActionsBtnClick;
+        useCapture = false;
+        var largeChatActionsBtnEl = getLargeChatActionsBtnEl();
+        if (largeChatActionsBtnEl) {
+            largeChatActionsBtnEl.addEventListener(type, listener, useCapture);
+            toDispose.push({ target: largeChatActionsBtnEl, type: type, listener: listener, useCapture: useCapture });
+            log('Subscribed to large chat actions button click.');
         }
 
         type = 'mousedown';
-        listener = onReplyBtnClick;
-        var replyBtn = getLargeChatReplyBtn();
-        if (replyBtn) {
-            replyBtn.addEventListener(type, listener, useCapture);
-            toDispose.push({ target: replyBtn, type: type, listener: listener, useCapture: useCapture });
-            log('Subscribed to reply button click.');
+        listener = onLargeChatReplyBtnClick;
+        var largeCharReplyBtnEl = getLargeChatReplyBtnEl();
+        if (largeCharReplyBtnEl) {
+            largeCharReplyBtnEl.addEventListener(type, listener, useCapture);
+            toDispose.push({ target: largeCharReplyBtnEl, type: type, listener: listener, useCapture: useCapture });
+            log('Subscribed to large chat reply button click.');
         }
+
+        log('Subscribed to events.');
     }
 
-    function onWindowKeydown(e) {
-        if (e.keyCode === 13) { // Enter.
-            tryStoreSmallChatWindowMessage();
-            tryStoreLargeChatWindowMessage();
-        }
-    }
-
-    function onChatTabsPageletKeydown(e) {
+    function onSmallChatKeydown(e) {
         if (e.keyCode === 13) { // Enter.
             tryStoreSmallChatWindowMessage();
         }
     }
 
-    function onPageletWebMessengerKeydown(e) {
+    function onLargeChatKeydown(e) {
         if (e.keyCode === 13) { // Enter.
             tryStoreLargeChatWindowMessage();
         }
     }
 
-    function onActionsBtnClick(e) {
+    function onSmallChatActionsBtnClick(e) {
+        var parentEl = e.target.parentNode;
+        if (parentEl.tagName === 'SPAN' || parentEl.classList.contains('openToggler')) {
+            tryStoreVisibleSmallChatMessages();
+        }
+    }
+
+    function onLargeChatActionsBtnClick(e) {
         var parentEl = e.target.tagName === 'BUTTON' ? e.target.parentNode : e.target.parentNode.parentNode;
         if (parentEl.classList.contains('openToggler')) {
-            tryStoreVisibleMessages();
+            tryStoreVisibleLargeChatMessages();
         }
     }
 
-    function onReplyBtnClick(e) {
+    function onLargeChatReplyBtnClick(e) {
         tryStoreLargeChatWindowMessage();
     }
 
@@ -130,31 +163,59 @@
     // Message extractors. //
     // ******************* //
 
-    function tryStoreVisibleMessages() {
-        log('Storing visible messages...');
-        var recentMessagesEl = document.getElementById('webMessengerRecentMessages');
-        if (recentMessagesEl) {
-            for (var i = 0; i < recentMessagesEl.children.length; i++) {
-                var childEl = recentMessagesEl.children[i];
-                if (childEl.classList.contains('webMessengerMessageGroup')) {
-                    var nameEl = childEl.querySelector('a[data-hovercard]');
-                    var name;
-                    if (nameEl) name = nameEl.innerText;
-                    var timestampEl = childEl.querySelector('abbr[data-utime]');
-                    var timestamp;
-                    if (timestampEl) timestamp = timestampEl.innerHTML;
+    function tryStoreVisibleSmallChatMessages() {
+        log('Storing visible small chat messages...');
 
-                    var messageEls = childEl.querySelectorAll('p');
-                    if (messageEls) {
-                        for (var j = 0; j < messageEls.length; j++) {
-                            var messageEl = messageEls[j];
-                            var message = messageEl.innerText;
-                            storeMessage(actionOnRecentMessagesStore, timestamp, name, message);
-                        }
-                    }
+        var recentMessageEls = getSmallChatEl().querySelectorAll('div.opened div.direction_ltr > div');
+        if (recentMessageEls.length) {
+            var myName = getMyName();
+            var otherNameEl = getSmallChatEl().querySelector('div.opened a.titlebarText > span');
+            var otherName = '';
+            if (otherNameEl) {
+                otherName = otherNameEl.innerText;
+            }
+
+            for (var i = 0; i < recentMessageEls.length; i++) {
+                var recentMessageEl = recentMessageEls[i];
+                if (recentMessageEl.innerText) {
+                    var computedStyle = getComputedStyle(recentMessageEl);
+                    var isMe = computedStyle.float === 'right';
+
+                    var textEl = recentMessageEl.querySelector('span > span');
+                    var message = recentMessageEl.innerText;
+                    var timestamp = getTimestamp();
+                    var name = isMe ? myName + ' -> ' + otherName : otherName + ' -> ' + myName;
+                    storeMessage(actionOnRecentMessagesStore, timestamp, name, message);
                 }
             }
-            log('Stored visible messages.');
+
+            log('Stored visible small chat messages.');
+        }
+        return false;
+    }
+
+    function tryStoreVisibleLargeChatMessages() {
+        log('Storing visible large chat messages...');
+        var recentMessageEls = document.querySelectorAll('#webMessengerRecentMessages li.webMessengerMessageGroup');
+        if (recentMessageEls.length) {
+            for (var i = 0; i < recentMessageEls.length; i++) {
+                var childEl = recentMessageEls[i];
+
+                var nameEl = childEl.querySelector('a[data-hovercard]');
+                var name;
+                if (nameEl) name = nameEl.innerText;
+                var timestampEl = childEl.querySelector('abbr[data-utime]');
+                var timestamp;
+                if (timestampEl) timestamp = timestampEl.innerHTML;
+
+                var messageEls = childEl.querySelectorAll('p');
+                for (var j = 0; j < messageEls.length; j++) {
+                    var messageEl = messageEls[j];
+                    var message = messageEl.innerText;
+                    storeMessage(actionOnRecentMessagesStore, timestamp, name, message);
+                }
+            }
+            log('Stored visible large chat messages.');
             return true;
         }
         return false;
@@ -162,23 +223,21 @@
 
     function tryStoreSmallChatWindowMessage() {
         log('Storing small chat window message...');
-        var chatTabsPageletEl = document.getElementById('ChatTabsPagelet') || document.body;
-        var activeChatTabEl = chatTabsPageletEl.querySelector('div.opened.focusedTab');
-        if (activeChatTabEl) {
-            var chatSpanEl = activeChatTabEl.querySelector('span[data-text="true"]');
-            if (chatSpanEl && chatSpanEl.innerText) {
-                var timestamp = getTimestamp();
-                var message = chatSpanEl.innerText;
-                var name = getMyName();
-                var titlebarTextEl = activeChatTabEl.querySelector('a.titlebarText > span');
-                if (titlebarTextEl) {
-                    var to = titlebarTextEl.innerText;
-                    name = name + ' -> ' + to;
-                }
-                storeMessage(sentMessagesStore, timestamp, name, message);
-                log('Stored small chat window message.');
-                return true;
+        var smallChatEl = getSmallChatEl();
+        var smallChatTabEl = smallChatEl && smallChatEl.querySelector('div.opened.focusedTab');
+        var chatEl = smallChatTabEl && smallChatTabEl.querySelector('span[data-text="true"]');
+        if (chatEl && chatEl.innerText) {
+            var timestamp = getTimestamp();
+            var message = chatEl.innerText;
+            var name = getMyName();
+            var titlebarTextEl = smallChatTabEl.querySelector('a.titlebarText > span');
+            if (titlebarTextEl) {
+                var to = titlebarTextEl.innerText;
+                name = name + ' -> ' + to;
             }
+            storeMessage(sentMessagesStore, timestamp, name, message);
+            log('Stored small chat window message.');
+            return true;
         }
         return false;
     }
@@ -190,8 +249,7 @@
             var timestamp = getTimestamp();
             var message = chatTextarea.value;
             var name = getMyName();
-            var webMessengerHeaderNameEl = document.getElementById('webMessengerHeaderName') || document.body;
-            var toEl = webMessengerHeaderNameEl.querySelector('a[data-hovercard]');
+            var toEl = document.querySelector('#webMessengerHeaderName a[data-hovercard]');
             if (toEl) {
                 name = name + ' -> ' + toEl.innerText;
             }
@@ -282,14 +340,20 @@
     // Providers. //
     // ********** //
 
+    function getSmallChatEl() { return document.getElementById('ChatTabsPagelet'); }
+    function getLargeChatEl() { return document.getElementById('pagelet_web_messenger'); }
+    function getSmallChatActionsBtnEl() { return document.querySelector('#ChatTabsPagelet span.optionMenu a[role="button"]'); }
+    function getLargeChatActionsBtnEl() { return document.querySelector('#pagelet_web_messenger div.uiPopover > button[type="submit"][value="1"]'); }
+    function getLargeChatReplyBtnEl() { return document.querySelector('#pagelet_web_messenger label.uiButton.uiButtonConfirm > input[type="submit"]'); }
+
     function sanitizeKey(key) {
         return key.replace('.', '_');
     }
 
     function getMyName() {
-        var profileAnchors = document.querySelectorAll('[data-testid="blue_bar_profile_link"]');
+        var profileAnchor = document.querySelector('[data-testid="blue_bar_profile_link"]');
         try {
-            return profileAnchors[0].children[1].innerText;
+            return profileAnchor.children[1].innerText;
         } catch(ex) {
             return undefined;
         }
@@ -297,14 +361,6 @@
 
     function getTimestamp() {
         return new Date().toLocaleString();
-    }
-
-    function getLargeChatActionsBtn() {
-        return document.querySelector('#pagelet_web_messenger div.uiPopover > button[type="submit"][value="1"]');
-    }
-
-    function getLargeChatReplyBtn() {
-        return document.querySelector('#pagelet_web_messenger label.uiButton.uiButtonConfirm > input[type="submit"]');
     }
 
     // *************** //
